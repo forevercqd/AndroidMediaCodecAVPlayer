5ba17000cdd13b3dffb76daad7437c35bd9e0b26
Robert <qingdong.cqd@alibaba-inc.com>
2019-05-05 23:52:26 +0800

    feat: 增android读写权限问题及音视频分流选择;

--- AVPlayer/app/src/main/java/com/devtips/avplayer/DemoMediaCodecActivity.java
+++ AVPlayer/app/src/main/java/com/devtips/avplayer/DemoMediaCodecActivity.java
@@ -1,15 +1,18 @@
 package com.devtips.avplayer;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +20,12 @@ import android.widget.TextView;
 
 import com.qmuiteam.qmui.widget.QMUITopBarLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Date;
 
 import static android.media.MediaCodec.BUFFER_FLAG_END_OF_STREAM;
 import static android.media.MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED;
@@ -43,10 +50,38 @@ public class DemoMediaCodecActivity extends Activity {
 
     private Handler mHandler;
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
+    private FileOutputStream pcmFileOutputSteam;
+    private FileOutputStream yuvFileOutputSteam;
+
+    ByteBuffer[] mDecodeOutputBuffers;
+
+    public static void verifyStoragePermissions(Activity activity) {
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_media_codec);
+
+
+        verifyStoragePermissions(this);
+
         mHandler = new Handler();
 
         mInfoTextView = findViewById(R.id.info_textview);
@@ -141,6 +176,22 @@ public class DemoMediaCodecActivity extends Activity {
             {
                 if (outIndex >= 0 && info.size > 0)
                 {
+                    try{
+                        mDecodeOutputBuffers = mediaCodec.getOutputBuffers();// MediaCodec将解码后的数据放到此ByteBuffer[]中
+                        ByteBuffer outBuffer = mDecodeOutputBuffers[outIndex];  // 拿到用于存放PCM数据的Buffer
+                        byte [] chunkPCM = new byte[info.size];// BufferInfo内定义了此数据块的大小
+                        outBuffer.get(chunkPCM);
+                        outBuffer.clear();// 数据取出后一定记得清空此Buffer MediaCodec是循环使用这些Buffer的，不清空下次会得到同样的数据
+
+                        if (pcmFileOutputSteam != null){
+                            pcmFileOutputSteam.write(chunkPCM);
+                        }
+                    }
+                    catch (Exception e){
+                        Log.e(TAG, "cqd, failed.", e);
+
+                    }
+
                     MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
                     bufferInfo.presentationTimeUs = info.presentationTimeUs;
                     bufferInfo.size = info.size;
@@ -174,33 +225,50 @@ public class DemoMediaCodecActivity extends Activity {
         // step 1：创建一个媒体分离器
         MediaExtractor extractor = new MediaExtractor();
         // step 2：为媒体分离器装载媒体文件路径
-        // 指定文件路径
-        Uri videoPathUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.img_video);
+        // 指定文件路径.
+//        Uri videoPathUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.img_video);
+        Uri videoPathUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.v1556618086240_1556618086309);
+
+        Log.d(TAG, "cqd, doDecoder, videoPathUri = " + videoPathUri);
         try {
             extractor.setDataSource(this, videoPathUri, null);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        Date date = new Date();
+        String pcmFilePath = Environment.getExternalStorageDirectory().getPath() + "/DCIM/" + date.getTime() + "_mediaCodec.pcm";
+        Log.d(TAG, "cqd, pcmFilePath = " + pcmFilePath);
+        try {
+            File file = new File(pcmFilePath);
+            pcmFileOutputSteam = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         // step 3：获取并选中指定类型的轨道
         // 媒体文件中的轨道数量 （一般有视频，音频，字幕等）
         int trackCount = extractor.getTrackCount();
         // mime type 指示需要分离的轨道类型
-        String extractMimeType = "video/";
+        String videoMimeType = "video/";
+        String audioMimeTyep = "audio/";
         MediaFormat trackFormat = null;
         // 记录轨道索引id，MediaExtractor 读取数据之前需要指定分离的轨道索引
-        int trackID = -1;
+        int videoTrackID = -1;
+        int audioTrackID = -1;
         for (int i = 0; i < trackCount; i++) {
             trackFormat = extractor.getTrackFormat(i);
-            if (trackFormat.getString(MediaFormat.KEY_MIME).startsWith(extractMimeType)) {
-                trackID = i;
-                break;
+            String key_mime = trackFormat.getString(MediaFormat.KEY_MIME);
+            if (key_mime.startsWith(videoMimeType)) {
+                videoTrackID = i;
+            }
+            else if(key_mime.startsWith(audioMimeTyep)){
+                audioTrackID = i;
             }
         }
         // 媒体文件中存在视频轨道
         // step 4：选中指定类型的轨道
-        if (trackID != -1)
-            extractor.selectTrack(trackID);
+        if (audioTrackID != -1)
+            extractor.selectTrack(audioTrackID);
 
         // step 5：根据 MediaFormat 创建解码器
         MediaCodec mediaCodec = null;
@@ -211,8 +279,7 @@ public class DemoMediaCodecActivity extends Activity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
+        
         while (true) {
             // step 6: 向解码器喂入数据
             boolean ret = feedInputBuffer(extractor,mediaCodec);
@@ -227,6 +294,12 @@ public class DemoMediaCodecActivity extends Activity {
         extractor.release();
         // 释放解码器
         mediaCodec.release();
+
+        try {
+            pcmFileOutputSteam.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
